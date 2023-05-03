d938e8a2a3b2da31cf62df5540d2bdb2ad61daee
Maria Meza <mmeza@licorne.mx>
2023-05-03 16:16:48 -0600

    ....

--- js/hero.js
+++ js/hero.js
@@ -2,17 +2,13 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
 
     navigation: {
-    nextEl: '.experts--next',
-    prevEl: '.experts--prev',
+    nextEl: '.hero--next',
+    prevEl: '.hero--prev',
     },
 
     pagination: {
         el: '.swiper-pagination',
       },
 
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
-    },
 
 })  
