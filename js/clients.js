301d86831696f8c6ba862a1ec247f06c5dd6e649
Maria Meza <mmeza@licorne.mx>
2023-05-03 10:09:23 -0600

    ...

--- js/clients.js
+++
@@ -1,9 +0,0 @@
-const swiperClients = new Swiper('.swiper_movil', {
-    slidesPerView: 1,
-
-    navigation: {
-    nextEl: '.clients_movil--next',
-    prevEl: '.clients_movil--prev',
-    },
-
-})  
