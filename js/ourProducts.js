a540b5dfc924a77223a96173ada71fe30f42a433
Maria Meza <mmeza@licorne.mx>
2023-05-04 09:05:22 -0600

    ...

---
+++ js/ourProducts.js
@@ -0,0 +1,10 @@
+const swiperProducts = new Swiper('.swiper_products', {
+    slidesPerView: 1,
+
+    navigation: {
+    nextEl: '.our_products__carousel__decoration__controls--next',
+    prevEl: '.our_products__carousel__decoration__controls--prev',
+    },
+
+
+})  
