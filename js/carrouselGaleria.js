2a0a5b50ef0816e61a12d5c01fb9f23eb82baafd
Maria Meza <mmeza@licorne.mx>
2023-05-05 15:18:17 -0600

    ...

--- js/carrouselGaleria.js
+++ js/carrouselGaleria.js
@@ -18,37 +18,35 @@ M.Carousel.init(carousel,{
 })
 
 
-
 function initialCarrouselFoto(){
-    M.Carousel.init(carousel,{
-        duration:150
-    })
-    
     carouselFotovoltaico.classList.remove('catalog__carousel_fotovoltaico--active')
     carouselTermico.classList.remove('catalog__carousel_termico--active')
     carouselIlumination.classList.remove('catalog__carousel_ilumination--active')
+
+    M.Carousel.init(carousel,{
+        duration:150,
+    })
+    
 }
 
 function initialCarrouselTermico(){
-    M.Carousel.init(carousel,{
-        duration:150
-    })
-    
     carouselFotovoltaico.classList.add('catalog__carousel_fotovoltaico--active')//*elimina
     carouselTermico.classList.add('catalog__carousel_termico--active')//*elimina
     carouselIlumination.classList.remove('catalog__carousel_ilumination--active')
+
+    M.Carousel.init(carousel,{
+        duration:150,       
+    })
 }
 
 function initialCarrouselIlumniation(){
-    M.Carousel.init(carousel,{
-        duration:150
-    })
-
     carouselFotovoltaico.classList.add('catalog__carousel_fotovoltaico--active')
     carouselTermico.classList.remove('catalog__carousel_termico--active')
     carouselIlumination.classList.add('catalog__carousel_ilumination--active')
 
-   
+    M.Carousel.init(carousel,{
+        duration:150,       
+    })
 }
 
 buttonFoto.addEventListener('click',initialCarrouselFoto )
