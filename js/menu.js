3463186a5e6458057eb0b0fe990144d877e1a2f5
Maria Meza <mmeza@licorne.mx>
2023-05-05 07:33:48 -0600

    ...

--- js/menu.js
+++ js/menu.js
@@ -4,6 +4,14 @@ const header = document.querySelector('.header')
 const toggle = document.querySelector('.header__mobile__top__toggle')
 const menu = document.querySelector('.menu')
 
+//*submenu Desktop
+const submenuButton = document.querySelector('.header__desktop__pages--two')
+const submenu = document.querySelector('.submenu')
+
+//*submenu movil
+const submenuButtonMovil = document.querySelector('.menu__link--two')
+const submenuMovil = document.querySelector('.menu__link__submenu')
+
 function toggleMenu() {
 	menu.classList.toggle('menu--active')
 	toggle.classList.toggle('header__mobile__top__toggle--active')
@@ -12,5 +20,12 @@ function DeleteMenu() {
 	menu.classList.remove('menu--active')
 }
 
+function SubmenuDesktop(){
+	submenu.classList.toggle('submenu--active')
+	submenuButton.classList.toggle('header__desktop__pages--two--active')
+}
+
+
 toggle.addEventListener('click', toggleMenu)
 menu.addEventListener('click', DeleteMenu)
+submenuButton.addEventListener('click', SubmenuDesktop)
