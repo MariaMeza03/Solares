6f0d9c4945342a011b1fda379b328d2c22240c8f
Maria Meza <mmeza@licorne.mx>
2023-05-08 16:46:13 -0600

    ...

---
+++ js/instalation.js
@@ -0,0 +1,12 @@
+const swiperProducts = new Swiper('.swiper_products_general', {
+
+    pagination: {
+        el: '.swiper-pagination',
+      },
+
+      autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+
+})  
