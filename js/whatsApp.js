6f1688c40d3c813d20ab40b3034f67502d0f3875
Maria Meza <mmeza@licorne.mx>
2023-05-15 13:56:46 -0600

    ...

--- js/whatsApp.js
+++
@@ -1,9 +0,0 @@
-//*WhatsApp
-const button = document.querySelector('.layout_whatsapp__button')
-const chat = document.querySelector('.chat_whatsApp')
-
-function toggleMenu() {
-	chat.classList.toggle('chat_whatsApp--active')
-}
-
-button.addEventListener('click', toggleMenu)
